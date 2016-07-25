eff3d84f51f6ca74e93324b9dadb9d174a17e9e5
Chris Scheller <schelcj@pobox.com>
2016-07-25 14:43:05 -0400

    quicky helper for tw

--- bash/.bash/task.sh
+++ bash/.bash/task.sh
@@ -5,3 +5,8 @@ alias waitingfor="task ls tag.has:waitingfor"
 alias today="task due:today list"
 alias tomorrow="task due:tomorrow list"
 alias overdue="task overdue"
+
+function postpone() {
+  local id=$1
+  task $id modify due:tomorrow
+}
